3c62e9049f7a6df6d068c3da9ae7f37601f83ccd
Stefan Boos <sboos@igus.net>
2023-06-01 17:55:21 +0200

    refactor: apply nice code style and change public to private
    ... we satisfy the complaints of the IDE
    
    Co-authored-by: Niklas Rueber <nrueber@igus.net>
    Co-authored-by: Bernhard Schenk <bernhardschenk@gmx.de>
    Co-authored-by: Atsuhiro Endo <atcheri@gmail.com>

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -5,59 +5,74 @@ namespace Tennis
         private const int FourPoints = 4;
         private const int SixPoints = 6;
         private int _player2Points;
-        private int player1Points;
-        private string player1Name;
-        private string player2Name;
-        public static readonly string[] POINTS_NAMES = new[] { "Love", "Fifteen", "Thirty", "Forty" };
+        private int _player1Points;
+        private readonly string _player1Name;
+        private readonly string _player2Name;
+        private static readonly string[] PointsNames = new[] { "Love", "Fifteen", "Thirty", "Forty" };
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            _player1Name = player1Name;
+            _player2Name = player2Name;
         }
 
         public string GetScore()
         {
             if (IsNotYetEndGame())
             {
-                var score = POINTS_NAMES[player1Points];
-                if (player1Points == _player2Points)
+                var score = PointsNames[_player1Points];
+                if (_player1Points == _player2Points)
+                {
                     return score + "-All";
+                }
                 else
-                    return score + "-" + POINTS_NAMES[_player2Points];
+                {
+                    return score + "-" + PointsNames[_player2Points];
+                }
             }
             else
             {
-                if (player1Points == _player2Points)
+                if (_player1Points == _player2Points)
+                {
                     return "Deuce";
+                }
+
                 if (PointsDifferenceIsOne())
+                {
                     return "Advantage " + LeadingPlayer();
+                }
                 else
+                {
                     return "Win for " + LeadingPlayer();
+                }
             }
         }
 
         private string LeadingPlayer()
         {
-            return player1Points > _player2Points ? player1Name : player2Name;
+            return _player1Points > _player2Points ? _player1Name : _player2Name;
         }
 
         private bool IsNotYetEndGame()
         {
-            return (player1Points < FourPoints && _player2Points < FourPoints) && (player1Points + _player2Points < SixPoints);
+            return (_player1Points < FourPoints && _player2Points < FourPoints) && (_player1Points + _player2Points < SixPoints);
         }
 
         private bool PointsDifferenceIsOne()
         {
-            return (player1Points - _player2Points) * (player1Points - _player2Points) == 1;
+            return (_player1Points - _player2Points) * (_player1Points - _player2Points) == 1;
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                this.player1Points += 1;
+            {
+                _player1Points += 1;
+            }
             else
-                this._player2Points += 1;
+            {
+                _player2Points += 1;
+            }
         }
 
     }
